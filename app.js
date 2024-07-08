993a49f94c861934b7e9e2d19b8f261ad120b8ea
kabsyaks kabeer <36848823+kabsyakskabeer@users.noreply.github.com>
2024-07-08 21:31:23 +0100

    formdata

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+self.addEventListener
+(
+    'fetch',(event)=>
+    {
+        if
+        (
+         event
+         .request
+         .method === 'POST'
+         && event.request.body
+       
+        )
+        {
+         const formData = 
+         event.request.body;
 
+         const d_name = 
+         formData
+         .get('d_name');
+
+         const d_unit =
+         formData
+         .get('d_unit');
+
+         const d_price =
+         formData
+         .get('d_price');
+
+         event.respondWith
+         ('form Data received')
+
+
+        }  
+    }
+);
