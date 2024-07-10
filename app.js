7766a2b1a5f8cf13d2e06c3039283f019ae47ba9
kabsyaks kabeer <36848823+kabsyakskabeer@users.noreply.github.com>
2024-07-10 19:14:25 +0100

    jk

--- app.js
+++ app.js
@@ -14,24 +14,12 @@ self.addEventListener
          const formData = 
          event.request.body;
 
-         const d_name = 
-         formData
-         .get('d_name');
-
-         const d_unit =
-         formData
-         .get('d_unit');
-
-         const d_price =
-         formData
-         .get('d_price');
-         console.log(d_price);
 
          event.respondWith
          (
             new Response
             (
-                d_name
+                'kabeer'
             )
          )
 
