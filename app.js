3e5680cd31392bb5e3ca389dcf710b755fb4791f
kabsyaks kabeer <36848823+kabsyakskabeer@users.noreply.github.com>
2024-07-11 22:11:18 +0100

    changes

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
 self.addEventListener
 (
-    'fetch',(event)=>
-    {
-        if
-        (
-         event
-         .request
-         .method === 'POST'
-         && event.request.body
-       
-        )
-        {
-         const formData = 
-         event.request.body;
-         console.log('happy man');
-
-
-         event.respondWith
-         (
-            new Response
-            (
-                'kabeer'
-            )
-         )
-
-
-        }  
-    }
+ 'message',
+  function (event) {
+    console.log(event.data);
+ }
 );
